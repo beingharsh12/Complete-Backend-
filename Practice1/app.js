ad961ffc7ecbcd13ebc4ce125aa209b0240d57c9
Harsh sharma <harshsharmamzn9412@gmail.com>
2026-06-19 12:17:01 +0530

    Practice1

---
+++ Practice1/app.js
@@ -0,0 +1,52 @@
+const http = require('http');
+
+    const server = http.createServer((req,res)=>{
+    console.log(req.url, req.method, req.headers);
+
+    if(req.url==='/home'){
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Myntra</title></head>')
+        res.write('<body>')
+       res.write('<h1>Welcome to the Home Page</h1>')
+        res.write('</body>')
+        res.write('</html>')
+       return res.end();
+    } else if(req.url==='/products'){
+         res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Myntra</title></head>')
+        res.write('<body>')
+       res.write('<h1>Welcome to the Products Page</h1>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end();
+    } else if(req.url==='/men'){
+         res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Myntra</title></head>')
+        res.write('<body>')
+       res.write('<h1>Welcome to the Mens Section</h1>')
+        res.write('</body>')
+        res.write('</html>')
+        return res.end();
+    } 
+
+       res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<head><title>Myntra</title></head>')
+        res.write('<body>')
+        res.write('<a href="/home">Home</a><br>')
+        res.write('<a href="/products">Products</a><br>')
+        res.write('<a href="/men">Men</a>')
+        res.write('</body>')
+        res.write('</html>')
+        res.end();
+
+})
+
+const PORT = 4000;
+server.listen(PORT, ()=>{
+      console.log(`Server is running on port http://localhost:${PORT}`);
+})
+
---
+++ Req & Res using Routes/user.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => { 
+    console.log(req.url, req.method, req.headers);
+    // process.exit(); // to stop the server after one request
+    if(req.url==='/'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My fist Node.js App</title></head>')
+    res.write('<body><h1>Enter the Details</h1>');
+    res.write('<form action="/submit-details" method="POST">')
+    res.write('<label for="name" >Name: </label>')
+    res.write('<input type="text" placeholder="Enter your name" id="name" name="name"/>')
+    res.write('<button type="submit">Submit</button>')
+    res.write('</form>')
+    res.write('</body>')
+    res.write('</html>');
+    return res.end();
+    } 
+    else if(req.method = 'POST' && req.url.toLowerCase()==='/submit-details'){
+        fs.writeFileSync('user-details.txt','Harsh Sharma');
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My fist Node.js App</title></head>')
+    res.write('<body><h1>Hello from my first Node.js App</h1></body>');
+    res.write('</html>');
+    res.end();
+});
+const PORT = 3000;
+
+
+
+server.listen(PORT, ()=>{
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
